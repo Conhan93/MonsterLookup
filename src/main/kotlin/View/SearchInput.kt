b9ad1d2669e17bdb10a72e3b29ce58662c27be15
Conny Hansson <conhan@hotmail.se>
2022-07-14 04:37:41 +0200

    Modify Searchfield
    Changed textfield to outlined, added placeholder text
    as well as search text to search button

--- src/main/kotlin/View/SearchInput.kt
+++ src/main/kotlin/View/SearchInput.kt
@@ -1,19 +1,18 @@
 package View
 
 import Service.MonsterService
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.TextField
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
 
@@ -21,26 +20,32 @@ import androidx.compose.ui.unit.dp
 fun SearchInput(monsterService : MonsterService, modifier : Modifier) {
 
     var name by remember { mutableStateOf("") }
-    Card(
-        modifier = modifier
-    ) {
-        Row {
-            TextField(
-                name,
-                onValueChange = {
-                    name = it
-                },
-                modifier = modifier
-            )
-            Spacer(Modifier.width(10.dp))
-            Button(
-                onClick =  {
-                    monsterService.monster.value = monsterService.getMonster(name)
-                },
-                modifier = modifier
-            ) {
 
-            }
+    val padding = PaddingValues(5.dp)
+
+    Row {
+
+        OutlinedTextField(
+            name,
+            onValueChange = {
+                name = it
+            },
+            modifier = modifier
+                .padding(padding),
+            shape = CutCornerShape(5.dp),
+            placeholder = { Text("Enter name of monster") },
+        )
+
+        Spacer(Modifier.width(10.dp))
+
+        Button(
+            onClick =  {
+                monsterService.monster.value = monsterService.getMonster(name)
+            },
+            modifier = modifier
+                .padding(padding)
+        ) {
+            Text("Search")
         }
     }
 
