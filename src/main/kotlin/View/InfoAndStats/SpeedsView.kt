601ba664d0a3b8b8f9924fb8af632218fa6d19cc
Conny Hansson <conhan@hotmail.se>
2022-07-15 19:50:48 +0200

    Speedview match senses
    Changed the appearance and code to match that of sense view

--- src/main/kotlin/View/InfoAndStats/SpeedsView.kt
+++ src/main/kotlin/View/InfoAndStats/SpeedsView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
@@ -14,43 +15,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 
 
 @Composable
 fun SpeedView(speed: Speed, modifier: Modifier = Modifier) {
 
-    Column(
-        modifier = modifier
-            .padding(15.dp)
-            .background(
-                color = Color.Gray
-            )
-            .verticalScroll(rememberScrollState())
+    val textSize = 0.8.em
+
+    @Composable
+    fun TextCell(text : String, modifier: Modifier = Modifier) =
+        Text(
+            text = text,
+            fontSize = textSize,
+            modifier = modifier
+        )
+
+    LazyColumn(
+        modifier = Modifier
+            .then(modifier)
     ) {
-        speed.walk?.let {
-            Box {
-                Text("Walking speed : ${speed.walk}")
-            }
+
+        item {
+            Text("Speeds")
         }
-        speed.burrow?.let {
-            Box {
-                Text("Burrowing speed : ${speed.burrow}")
-            }
-        }
-        speed.climb?.let {
-            Box {
-                Text("Climbing speed : ${speed.climb}")
-            }
-        }
-        speed.fly?.let {
-            Box {
-                Text("Flying speed : ${speed.fly}")
-            }
-        }
-        speed.swim?.let {
-            Box {
-                Text("Swimming speed : ${speed.swim}")
-            }
+
+        item {
+            speed.walk?.let { TextCell("Walking: $it") }
+            speed.burrow?.let { TextCell("Burrowing: $it") }
+            speed.swim?.let { TextCell("Swimming: $it") }
+            speed.climb?.let { TextCell("Climbing: $it") }
+            speed.fly?.let {TextCell("Flying: $it")}
         }
     }
 }
