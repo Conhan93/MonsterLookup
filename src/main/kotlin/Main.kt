da21ced5a1213da403303f1c61ca9ee87a77f6e5
Conny Hansson <conhan@hotmail.se>
2022-07-15 17:47:17 +0200

    Set Window size
    Sets window size at start up and adds monster lookup as
    title

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,8 +15,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 @Composable
@@ -85,7 +87,15 @@ fun main() = application {
 
     val monsterService = MonsterService()
 
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = "Monster Lookup",
+        state = WindowState(
+            width = 1300.dp,
+            height = 600.dp
+        ),
+        onCloseRequest = ::exitApplication
+
+    ) {
         App(monsterService)
     }
 }
