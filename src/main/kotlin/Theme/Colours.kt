c1472278706b9de3c063f076c0c2fd0958f13863
Conny Hansson <conhan@hotmail.se>
2022-07-23 09:40:52 +0200

    Add colour theme
    Implement a basic colour theme, using compose darkColors

---
+++ src/main/kotlin/Theme/Colours.kt
@@ -0,0 +1,20 @@
+package Theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val darkColours = darkColors(
+    primary = Color(0xff200b0b),
+    primaryVariant = Color(0xff2f0000),
+    secondary = Color(0xff490000),
+    secondaryVariant = Color(0xff650000),
+    background = Color(0xff0f0606),
+    onPrimary = Color.White,
+    onSecondary = Color.Yellow,
+    onBackground = Color.Red
+)
+
+val lightColours = lightColors(
+
+)
