fbd6133fa1452c99750f47e0e879029915e06b3f
Conny Hansson <conhan@hotmail.se>
2022-07-26 01:08:04 +0200

    Align characterInfo
    ALigned the elements in the characterinfo view to each other and reduced the font size in the about view

--- src/main/kotlin/View/InfoAndStats/CharacterInfo.kt
+++ src/main/kotlin/View/InfoAndStats/CharacterInfo.kt
@@ -10,54 +10,86 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 
 @Preview
 @Composable()
 fun CharacterInfo(character : Monster, modifier : Modifier = Modifier) {
 
-    Box(
-        modifier = modifier
-    ) {
-        Row {
+
+        Row(
+            modifier = Modifier
+                .padding(15.dp)
+                .then(modifier)
+        ) {
             Column(
                 modifier = Modifier
-                    .padding(15.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Row {
                     Column {
                         Text("Name : ${character.name ?: "Unavailable"}")
                         Column { Text(text = character.desc.joinToString()) }
                     }
+                }
+                Stats(character = character)
 
-                }
-                Row {
-                    Text("STR : ${character.strength ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-                    Text("CON : ${character.constitution ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-                    Text("WIS : ${character.wisdom ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-
-                }
-                Row {
-                    Text("DEX : ${character.dexterity ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-                    Text("INT : ${character.intelligence ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-                    Text("CHA : ${character.intelligence ?: "N/A" }")
-                    Spacer(Modifier.width(10.dp))
-                }
             }
 
-            Column {
-                Text("Type : ${character.type ?: "N/A"}")
-                character.subtype?.let { Text("Subtype : ${character.subtype}") }
-                Text("Size : ${character.size ?: "N/A"}")
-                Text("Alignment : ${character.alignment ?: "N/A"}")
-                Text("AC : ${character.armor_class ?: "N/A"}")
-                Text("HP : ${character.hit_points ?: "N/A"}")
-            }
+            About(
+                character = character,
+                modifier = Modifier.fillMaxHeight()
+            )
         }
+}
 
+@Composable
+fun Stats(character: Monster, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row {
+            Text("STR : ${character.strength ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+            Text("CON : ${character.constitution ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+            Text("WIS : ${character.wisdom ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+
+        }
+        Row {
+            Text("DEX : ${character.dexterity ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+            Text("INT : ${character.intelligence ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+            Text("CHA : ${character.intelligence ?: "N/A" }")
+            Spacer(Modifier.width(10.dp))
+        }
+    }
+}
+
+@Composable
+fun About(character: Monster, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+        val textSize = 0.7.em
+
+        @Composable
+        fun TextCell(text : String, modifier: Modifier = Modifier) =
+            Text(
+                text = text,
+                fontSize = textSize,
+                modifier = modifier
+            )
+
+        TextCell("Type : ${character.type ?: "N/A"}")
+        character.subtype?.let { TextCell("Subtype : ${character.subtype}") }
+        TextCell("Size : ${character.size ?: "N/A"}")
+        TextCell("Alignment : ${character.alignment ?: "N/A"}")
+        TextCell("AC : ${character.armor_class ?: "N/A"}")
+        TextCell("HP : ${character.hit_points ?: "N/A"}")
     }
 }
