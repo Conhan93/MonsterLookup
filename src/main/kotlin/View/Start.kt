6d1ba39af30eb5e92717e0da7e62f00782fff923
Conny Hansson <conhan@hotmail.se>
2022-07-26 03:55:14 +0200

    Start alignment and surface
    Wrap start page in surface to provide background colour and correct text contrast colour. Use column to center search box

--- src/main/kotlin/View/Start.kt
+++ src/main/kotlin/View/Start.kt
@@ -4,31 +4,53 @@ import Model.Monster.Monster
 import Service.MonsterService
 import State.State
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Start(
     state : MutableState<State<Monster>?>,
     monsterService : MonsterService
 ) {
-    Box(
+    Surface(
         modifier = Modifier
-            .fillMaxSize(1f)
-            .background(
-                color = Color.LightGray
-            ),
-        contentAlignment = Alignment.Center
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
     ) {
-        SearchInput(
-            state = state,
-            monsterService = monsterService
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(
+                        color = MaterialTheme.colors.primary,
+                        shape = RoundedCornerShape(15.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                SearchInput(
+                    state = state,
+                    monsterService = monsterService
+                )
+            }
+        }
+
     }
+
 
 }
