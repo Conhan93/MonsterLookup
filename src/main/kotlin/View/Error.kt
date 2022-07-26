5c270cd56c3a43c43867b4103665d51151f0b5fb
Conny Hansson <conhan@hotmail.se>
2022-07-26 06:21:43 +0200

    Error alignment and surface
    Wrap Error page in surface to provide background colour and correct text contrast colour. Use column to center search box

--- src/main/kotlin/View/Error.kt
+++ src/main/kotlin/View/Error.kt
@@ -4,16 +4,17 @@ import Model.Monster.Monster
 import Service.MonsterService
 import State.State
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Error(
@@ -21,21 +22,39 @@ fun Error(
     state : MutableState<State<Monster>?>,
     monsterService: MonsterService
 ) {
-
-    Column(
+    Surface(
         modifier = Modifier
-            .fillMaxSize()
-            .background(
-                color = Color.LightGray
-            ),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
     ) {
-        //Text(state.message ?: "N/A")
-        Text(error.error.localizedMessage)
-        SearchInput(
-            state = state,
-            monsterService = monsterService
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(
+                        color = MaterialTheme.colors.primary,
+                        shape = RoundedCornerShape(15.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = error.error.localizedMessage,
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .padding(vertical = 10.dp)
+                )
+                SearchInput(
+                    state = state,
+                    monsterService = monsterService,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                )
+            }
+        }
     }
 }
