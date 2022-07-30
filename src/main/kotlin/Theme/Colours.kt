a066e1fe99b8dac3e3b849497182c77f02e4af0e
Conny Hansson <conhan@hotmail.se>
2022-07-30 10:11:41 +0200

    Add colours
    Add colours for both light and dark colour palettes

--- src/main/kotlin/Theme/Colours.kt
+++ src/main/kotlin/Theme/Colours.kt
@@ -4,17 +4,35 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Colours and colour names generated with coolors.co
+ */
+
+
+val jet = Color(0xFF2D2D2A)
+val goldenBrown = Color(0xFF99672E)
+val darkOchre = Color(0xFFC17926)
+val darkCornFlowerBlue = Color(0xF0153D7D)
+val queenBlue = Color(0xFF4B6582)
+
 val darkColours = darkColors(
-    primary = Color(0xff200b0b),
-    primaryVariant = Color(0xff2f0000),
-    secondary = Color(0xff490000),
-    secondaryVariant = Color(0xff650000),
-    background = Color(0xff0f0606),
-    onPrimary = Color.White,
-    onSecondary = Color.Yellow,
-    onBackground = Color.Red
+    primary = darkOchre,
+    primaryVariant = goldenBrown,
+    secondary = queenBlue,
+    secondaryVariant = darkCornFlowerBlue,
+    background = jet,
 )
 
-val lightColours = lightColors(
+val lightOchre = Color(0xFFD27C19)
+val bronze = Color(0xFFD58F3F)
+val ceruleanBlue = Color(0xF03B60E8)
+val glacous = Color(0xFF6280DB)
+val alabaster = Color(0xFFEEECE4)
 
+val lightColours = lightColors(
+    primary = lightOchre,
+    primaryVariant = bronze,
+    secondary = glacous,
+    secondaryVariant = ceruleanBlue,
+    background = alabaster,
 )
