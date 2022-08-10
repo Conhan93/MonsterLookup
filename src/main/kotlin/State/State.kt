b086ef7d01f1574e407a3e3b0f8e7b9386c15d38
Conny Hansson <conhan@hotmail.se>
2022-08-10 10:25:15 +0200

    State error takes message
    State error now takes message string
    as argument in constructor. Lets state hold error messages that can be passed around to UI

--- src/main/kotlin/State/State.kt
+++ src/main/kotlin/State/State.kt
@@ -3,9 +3,8 @@ package State
 import Model.Monster.Monster
 import Model.Spell.Spell
 
-
 sealed class State {
     object Loading : State()
     data class Content(val monster: Monster? = null, val spell: Spell? = null) : State()
-    data class Error(val error : Throwable) : State()
+    data class Error(val message : String, val cause : Throwable? = null) : State()
 }
