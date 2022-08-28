9a36121bb3a1ebb0db658e168223a926b728e835
Conny Hansson <conhan@hotmail.se>
2022-08-28 14:06:28 +0200

    Spell detail
    Implement spell detail as a popup on special abilities, where clicking on a
    special ability with spells embedded in it will open a fullscreen popup
    displaying a list of the spells with more details about them.

--- src/main/kotlin/View/ActionsAndAbilities/SpecialAbilitiesView.kt
+++ src/main/kotlin/View/ActionsAndAbilities/SpecialAbilitiesView.kt
@@ -1,15 +1,25 @@
 package View
 
+import Model.Base.APIReference
 import Model.Monster.Monster
 import Model.Monster.SpecialAbilities
+
+import View.ActionsAndAbilities.SpellDetail
+import View.Common.*
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -44,14 +54,18 @@ fun SpecialAbilitiesView(monster: Monster, modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SpecialAbilityItem(ability : SpecialAbilities, modifier: Modifier = Modifier) {
+
+    var isClicked by remember { mutableStateOf(false) }
 
     Surface(
         modifier = modifier,
         color = MaterialTheme.colors.primary,
         shape = RoundedCornerShape(4.dp),
-        elevation = 2.dp
+        elevation = 2.dp,
+        onClick = { isClicked = isClicked.not() }
     ) {
         Column(
             modifier = Modifier.padding(5.dp)
@@ -71,6 +85,25 @@ fun SpecialAbilityItem(ability : SpecialAbilities, modifier: Modifier = Modifier
             ability.spellcasting.let {
                 it.school?.let { it1 -> Text(it1) }
             }
+        }
+    }
+
+    // Display popup with details of spell in special ability
+    if (isClicked && ability.spellcasting.spells.isNotEmpty()) {
+        FullScreenPopup(innerBoxSize = 0.7f) {
+            SpellDetail(
+                references = ability
+                    .spellcasting
+                    .spells
+                    .map {
+                        APIReference(
+                            name = it.name,
+                            url = it.url
+                        )
+                    },
+                modifier
+                    .align(Alignment.Center)
+            )
         }
     }
 }
---
+++ src/main/kotlin/View/ActionsAndAbilities/SpellDetailView.kt
@@ -0,0 +1,140 @@
+package View.ActionsAndAbilities
+
+import Model.Base.APIReference
+import Model.Spell.Spell
+import Service.ContentService
+import Service.SpellContentService
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun SpellDetail(
+    references: List<APIReference>,
+    modifier : Modifier = Modifier
+) {
+    val listOfSpells = mutableStateListOf<Spell>()
+
+    fetchSpells(references) { listOfSpells.add(it) }
+
+    backgroundSurface(modifier) {
+        contentBackground {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                item { Text(
+                    text = "Spells",
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+                }
+                items(listOfSpells) {
+                    SpellView(
+                        spell = it,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SpellView(
+    spell: Spell,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = Modifier
+            .then(modifier),
+        shape = RoundedCornerShape(3.dp),
+        color = MaterialTheme.colors.primary,
+        elevation = 3.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(5.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            spell.name?.let { Text(it) }
+
+            if (spell.desc.isNotEmpty()) {
+                Text(spell.desc.joinToString(separator = System.lineSeparator()))
+            }
+        }
+    }
+}
+
+@Composable
+private fun backgroundSurface(
+    modifier: Modifier = Modifier,
+    colour : Color = MaterialTheme.colors.background,
+    elevation : Dp = 10.dp,
+    content : @Composable () -> Unit
+) = Surface(
+    Modifier
+        .then(modifier) // apply alignment from popup
+        .padding(horizontal = 20.dp, vertical = 10.dp),
+    color = colour,
+    shape = RoundedCornerShape(15.dp),
+    elevation = elevation
+) { content() }
+
+@Composable
+private fun contentBackground(
+    modifier: Modifier = Modifier,
+    colour : Color = MaterialTheme.colors.primaryVariant,
+    elevation : Dp = 10.dp,
+    content: @Composable () -> Unit
+) = Surface(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .then(modifier),
+    color = colour,
+    shape = RoundedCornerShape(15.dp),
+    elevation = elevation
+) { content() }
+
+
+private fun fetchSpells(
+    references : List<APIReference>,
+    onFetch : (spell : Spell) -> Unit
+) {
+    // Coroutine scope for the http request
+    val requestScope = CoroutineScope(Dispatchers.IO)
+    val service: ContentService = SpellContentService() // TODO should use DI
+
+    println("Getting spell")
+
+    requestScope.launch {
+        references.forEach {
+            (service.getContent(it) as State.State.Content).spell?.let { it1 -> onFetch(it1) }
+        }
+    }
+}
