32d60fb22d221104cf5ce91d80570d49e2ec3ec6
Conny Hansson <conhan@hotmail.se>
2022-09-01 12:45:48 +0200

    Update Kotlin and Compose
    Updates Kotlin version from 1.6.10 to latest 1.7.10
    Updates Compose version from 1.1.0 to 1.2.0-alpha01-dev774

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.0"
+    kotlin("jvm") version "1.7.10"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev774"
     kotlin("plugin.serialization") version "1.6.10"
 }
 
@@ -45,7 +45,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.jvmTarget = "17"
 }
 
 compose.desktop {
