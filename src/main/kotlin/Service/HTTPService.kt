7e9de8dc254af22148cefe4163d350fa3d9ff4e4
Conny Hansson <conhan@hotmail.se>
2022-09-21 16:59:03 +0200

    Http Service build request
    Implement additional functionality in http service to build http requests by
    mapping the class and name/url parameter to the correct api and building them

--- src/main/kotlin/Service/HTTPService.kt
+++ src/main/kotlin/Service/HTTPService.kt
@@ -1,10 +1,19 @@
 package Service
 
+import Model.Base.APIReference
+
+import Util.API
+import Util.formatSearchName
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
+import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+
+import kotlin.reflect.KClass
 
 /**
  * Implements http functionality for content services
@@ -37,4 +46,46 @@ interface HTTPService {
 
         onSucceed?.let { it(response) }
     }
+
+    /**
+     * Uses the name and the class provided to build a http request
+     * to the api
+     * @param name of the target
+     * @param klass the class of the target
+     *
+     * @return http request to the API for the target
+     */
+    fun buildRequest(name: String, klass : KClass<*>) : HttpRequest {
+        return HttpRequest
+            .newBuilder()
+            .uri(URI(API.paths[klass] + name.formatSearchName()))
+            .build()
+    }
+
+    /**
+     * Uses the url in the reference and the class provided to build
+     * a http request to the api
+     *
+     * @param reference containing the url to the target
+     * @param klass the class of the target
+     *
+     * @return http request to the API for the target
+     */
+    fun buildRequest(reference : APIReference, klass: KClass<*>) : HttpRequest {
+        val url = if (!reference.url.isNullOrEmpty())
+            reference.url
+        else
+            throw IllegalArgumentException("No Url")
+
+        if (!url.startsWith((API.paths[klass] ?: "").removePrefix(API.baseURL)))
+            throw IllegalArgumentException("Bad URL $url")
+
+        return HttpRequest.newBuilder()
+            .uri(URI("${API.baseURL}$url"))
+            .GET()
+            .build()
+    }
 }
+
+
+
