8591307599efbe68cea5eb0f1390c002396e643b
Conny Hansson <conhan@hotmail.se>
2022-09-21 16:51:39 +0200

    API map
    Map api paths to their respective model classes

--- src/main/kotlin/Util/API.kt
+++ src/main/kotlin/Util/API.kt
@@ -1,8 +1,17 @@
 package Util
 
+import Model.Monster.Monster
+import Model.Spell.Spell
+import kotlin.reflect.KClass
+
 object API {
     const val baseURL = "https://www.dnd5eapi.co"
     const val baseAPI = "https://www.dnd5eapi.co/api"
     const val APIMonsterPath = "https://www.dnd5eapi.co/api/monsters/"
     const val APISpellsPath = "https://www.dnd5eapi.co/api/spells/"
+
+    val paths : Map<KClass<*>, String> = mapOf(
+        Spell::class to APISpellsPath,
+        Monster::class to APIMonsterPath
+    )
 }
