4f8a2a6d0941283458c45ff0310de1ea9498ea73
Conny Hansson <conhan@hotmail.se>
2022-09-24 20:34:27 +0200

    Content module provides dice service
    Module provides a dice service factory

--- src/main/kotlin/DI/ContentModule.kt
+++ src/main/kotlin/DI/ContentModule.kt
@@ -2,11 +2,13 @@ package DI
 
 import Service.ContentService
 import Service.ContentServiceImpl
+import Service.DiceService
+import Service.DiceServiceImpl
 
 import org.koin.dsl.module
 
 
 val contentModule = module {
-
+    factory<DiceService> { DiceServiceImpl() }
     single<ContentService> { ContentServiceImpl(storage = get()) }
 }
